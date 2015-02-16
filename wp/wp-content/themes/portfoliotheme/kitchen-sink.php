69bb84c1113052701966276cd478f8a133f2485d
reng-v1 <reng@ninjamultimedia.com>
2015-02-16 13:05:04 -0500

    add kitchen sink template

---
+++ wp/wp-content/themes/portfoliotheme/kitchen-sink.php
@@ -0,0 +1,62 @@
+<?php 
+/*
+Template Name: KitchenSink Template
+*/
+get_header(); ?>
+
+<div class="headings">
+  <h1><a href="#">Heading 1</a></h1>
+  <h2><a href="#">Heading 2</a></h2>
+  <h3><a href="#">Heading 3</a></h3>
+  <h4><a href="#">Heading 4</a></h4>
+  <h5><a href="#">Heading 5</a></h5>
+  <h6><a href="#">Heading 6</a></h6>
+</div>
+
+<div class"body-content">
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel velit dui. Vestibulum vel nibh lorem. Duis scelerisque leo leo, a tempus ante posuere eget. Nam ut vulputate metus. Suspendisse mi enim, laoreet in ligula a, pharetra placerat lorem. Suspendisse sit amet tellus erat. Sed rhoncus dictum <a href="#">This is a link</a> vestibulum. Nullam at tempor dui. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos...</p>
+  <div class="category-title">
+    <p>Category:</p>
+  </div>
+  <div class="category-list">
+    <p><a href="#">Responsive Design</a>, <a href="#">Wordpress</a>, <a href="#">SCSS</a>, <a href="#">PHP</a></p>
+  </div>
+</div>
+
+<div class="lists">
+  <ul>
+    <li class="body-list"><a href="#">List Item Here</a></li>
+    <li class="body-list"><a href="#">List Item Here</a></li>
+    <li class="body-list"><a href="#">List Item Here</a></li>
+    <li class="body-list"><a href="#">List Item Here</a></li>
+  </ul>
+</div>
+
+<div class="related-project">
+  <div>
+    <img src="http://www.fillmurray.com/g/340/250">
+    <div class="related-overlay"></div>
+    <h5>Project Name Goes Here</h5>
+  </div>
+  <div>
+    <img src="http://www.fillmurray.com/g/340/250">
+    <div class="related-overlay"></div>
+    <h5>Project Name Goes Here</h5>
+  </div>
+  <div>
+    <img src="http://www.fillmurray.com/g/340/250">
+    <div class="related-overlay"></div>
+    <h5>Project Name Goes Here</h5>
+  </div>
+</div>
+
+<div class="social">
+  <div class="icons">
+    <a href="#"><i class="linkedin grow-rotate ion-social-linkedin"></i></a>
+    <a href="#"><i class="twitter grow-rotate ion-social-twitter"></i></a>
+    <a href="#"><i class="github grow-rotate ion-social-github"></i></a>
+    <a href="#"><i class="codepen grow-rotate ion-social-codepen"></i></a>
+  </div>
+</div>
+
+<?php get_footer(); ?>
