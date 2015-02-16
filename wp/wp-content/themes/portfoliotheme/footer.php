27aa467ce562ecf2fd24c3d17e53e398c0977ba4
reng-v1 <reng@ninjamultimedia.com>
2015-02-16 17:55:42 -0500

    build out and styled footer

--- wp/wp-content/themes/portfoliotheme/footer.php
+++ wp/wp-content/themes/portfoliotheme/footer.php
@@ -13,6 +13,44 @@
 
   <div class="footer-wrapper">
   	
+    <div class="center-wrapper">
+
+      <div class="grid-row">
+        <div class="col small-col-12">
+          <h1>Contact Me</h1>
+        </div>
+      </div>
+
+      <div class="grid-row">
+
+        <div class="col small-col-12 medium-col-6">
+          <div class="contact-section">
+            <?php echo do_shortcode('[contact-form-7 id="18" title="Footer Contact"]'); ?>
+          </div>
+        </div>
+
+        <div class="col small-col-12 medium-col-6">
+          <div class="about-section">
+            <div class="about-image">
+              <img src="<?php echo get_stylesheet_directory_uri().'/images/about-me-pic.jpg'?>" />
+            </div>
+            <div class="about-content">
+              <h2>About Me</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu purus vehicula, finibus nisi sit amet, viverra leo. In dapibus felis sit amet sollicitudin ultrices. Quisque venenatis mollis ligula, sit amet viverra est sagittis ut. Morbi a massa vitae magna consequat sodales non nec erat.</p>
+            </div>
+            <h1><a class="emailaddress" href="">ryanengkl@gmail.com</a></h1>
+            <div class="icons-wrapper">
+              <a href="#" class="icon"><i class="linkedin grow-rotate ion-social-linkedin"></i></a>
+              <a href="#" class="icon"><i class="twitter grow-rotate ion-social-twitter"></i></a>
+              <a href="#" class="icon"><i class="github grow-rotate ion-social-github"></i></a>
+              <a href="#" class="icon"><i class="codepen grow-rotate ion-social-codepen"></i></a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
   </div>
 
 
