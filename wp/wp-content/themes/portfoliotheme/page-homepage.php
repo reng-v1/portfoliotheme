f43b1699b50f34009744b1de9780785f7e56a251
reng-v1 <reng@ninjamultimedia.com>
2015-02-18 00:33:51 -0500

    build to desktop size of work/homepage

---
+++ wp/wp-content/themes/portfoliotheme/page-homepage.php
@@ -0,0 +1,112 @@
+<?php 
+/*
+Template Name: Homepage
+*/
+get_header(); ?>
+
+<div class="grid-row">
+	<div class="col small-col-12">
+		<div id='project-slider' class='swipe'>
+		  <div class='swipe-wrap'>
+		    <div class="slide">
+		    	<div class="grid-row">
+		    		<div class="col small-col-12 medium-col-4">
+		    			<div class="project-title">
+		    				<h2>Project Title</h2>
+		    			</div>
+		    		</div>
+		    		<div class="col small-col-12 medium-col-8">
+		    			<div class="fade-gallery">
+		    				<div class="image-wrapper">
+		    					<img class="image image-1" src="http://placehold.it/500x300&text=Image1" />
+		    					<img class="image image-2" src="http://placehold.it/500x300&text=Image2" />
+		    					<img class="image image-3" src="http://placehold.it/500x300&text=Image3" />
+		    					<img class="image image-4" src="http://placehold.it/500x300&text=Image4" />
+		    					<img class="image image-5" src="http://placehold.it/500x300&text=Image5" />
+		    				</div>
+		    				<div class="thumbnail-wrapper">
+		    					<div class="thumbnail thumbnail-1">
+		    						<div class="overlay"></div>
+		    						<img src="http://placehold.it/250x150&text=Image1" />
+		    					</div>
+		    					<div class="thumbnail thumbnail-2">
+		    						<div class="overlay"></div>
+		    						<img src="http://placehold.it/250x150&text=Image2" />
+		    					</div>
+		    					<div class="thumbnail thumbnail-3">
+		    						<div class="overlay"></div>
+		    						<img src="http://placehold.it/250x150&text=Image3" />
+		    					</div>
+		    					<div class="thumbnail thumbnail-4">
+		    						<div class="overlay"></div>
+		    						<img src="http://placehold.it/250x150&text=Image4" />
+		    					</div>
+		    					<div class="thumbnail thumbnail-5">
+		    						<div class="overlay"></div>
+		    						<img src="http://placehold.it/250x150&text=Image5" />
+		    					</div>
+		    				</div>
+		    				<div class="text-wrapper">
+		    					<div class="text text-1">
+		    						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel velit dui. Vestibulum vel nibh lorem. Duis scelerisque leo leo, a tempus ante posuere eget. Nam ut vulputate metus. Suspendisse mi enim, laoreet in ligula a, pharetra placerat lorem. Suspendisse sit amet tellus erat. Sed rhoncus dictum vestibulum. Nullam at tempor dui. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+		    					</div>
+		    					<div class="text text-2">
+		    						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel velit dui. Vestibulum vel nibh lorem. Duis scelerisque leo leo, a tempus ante posuere eget. Nam ut vulputate metus. Suspendisse mi enim, laoreet in ligula a, pharetra placerat lorem. Suspendisse sit amet tellus erat. Sed rhoncus dictum vestibulum. Nullam at tempor dui. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+		    					</div>
+		    					<div class="text text-3">
+		    						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel velit dui. Vestibulum vel nibh lorem. Duis scelerisque leo leo, a tempus ante posuere eget. Nam ut vulputate metus. Suspendisse mi enim, laoreet in ligula a, pharetra placerat lorem. Suspendisse sit amet tellus erat. Sed rhoncus dictum vestibulum. Nullam at tempor dui. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+		    					</div>
+		    					<div class="text text-4">
+		    						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel velit dui. Vestibulum vel nibh lorem. Duis scelerisque leo leo, a tempus ante posuere eget. Nam ut vulputate metus. Suspendisse mi enim, laoreet in ligula a, pharetra placerat lorem. Suspendisse sit amet tellus erat. Sed rhoncus dictum vestibulum. Nullam at tempor dui. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+		    					</div>
+		    					<div class="text text-5">
+		    						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel velit dui. Vestibulum vel nibh lorem. Duis scelerisque leo leo, a tempus ante posuere eget. Nam ut vulputate metus. Suspendisse mi enim, laoreet in ligula a, pharetra placerat lorem. Suspendisse sit amet tellus erat. Sed rhoncus dictum vestibulum. Nullam at tempor dui. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+		    					</div>
+		    				</div>
+		    			</div>
+		    			<div class="grid-row">
+		    				<div class="col small-col-12 medium-col-8">
+		    					<div class="category-title">
+							    	<p>Category:</p>
+							    </div>
+							    <div class="category-list">
+							    	<p><a href="#">Responsive Design</a>, <a href="#">Wordpress</a>, <a href="#">SCSS</a>, <a href="#">PHP</a></p>
+							  	</div>
+		    				</div>
+		    				<div class="col small-col-12 medium-col-4">
+		    					<a href="#" class="site-btn">Live Site</a>
+		    				</div>
+		    			</div>
+		    		</div>
+		    	</div>
+		    </div>
+		  </div>
+		</div>
+	</div>
+</div>
+<div class="grid-row">
+	<div class="col small-col-12">
+		<h3 class="related-header">Related Projects</h3>
+	</div>
+	<div class="col small-col-12">
+		<div class="related-project">
+		  <div>
+		    <img src="http://www.fillmurray.com/g/340/250">
+		    <div class="related-overlay"></div>
+		    <h5>Project Name Goes Here</h5>
+		  </div>
+		  <div>
+		    <img src="http://www.fillmurray.com/g/340/250">
+		    <div class="related-overlay"></div>
+		    <h5>Project Name Goes Here</h5>
+		  </div>
+		  <div>
+		    <img src="http://www.fillmurray.com/g/340/250">
+		    <div class="related-overlay"></div>
+		    <h5>Project Name Goes Here</h5>
+		  </div>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
